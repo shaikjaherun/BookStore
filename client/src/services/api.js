18078e439aab6129b4e093acaac51029c5d39391
Shaik Jaherun <skjaherun25@gmail.com>
2026-07-08 23:15:24 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
+export default API;
 
 // Attach JWT token automatically
 API.interceptors.request.use(
